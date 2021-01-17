96e8ca8d6677cbcf0f732c85c81ffc3af5adc192
hardikphalet <hardik.phalet@gmail.com>
2021-01-17 22:29:48 +0530

    Added a zeros finding algorithms (Newton Raphson method)

--- Vigilant/Vigilant.cpp
+++ Vigilant/Vigilant.cpp
@@ -4,9 +4,9 @@
 int main()
 {
     std::cout << "Vigilant starting..." << std::endl;
-    Polynomial poly(0);
+    Polynomial poly(2);
     double arr[] = {
-        1
+        1.0, -2.0, 1.0
     };
     poly.setCoeff(arr);
     std::cout << "Value of polynomial at 1 is " << poly.valueAt(1.1) << std::endl;
@@ -14,6 +14,7 @@ int main()
                                               
     std::cout << "Value of derivative at 1 is " << poly.derivative().valueAt(1) << std::endl;
     std::cout << "Value of derivative at 1 is " << poly.derivative().valueAt(2) << std::endl;
+    std::cout << "Zero of the given Polynomial is " << poly.newtonRaphson(1.1) << std::endl;
 
     std::cout << "Program has ended. Please press enter to exit." << std::endl;
     std::cin.get();
